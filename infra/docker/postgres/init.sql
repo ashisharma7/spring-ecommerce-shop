119641a99c52ce17617b80c16f3009e038012689
Ashish Sharma <asishshrma7@gmail.com>
2025-12-22 18:28:24 +0530

    chore(infra): fix postgres port conflict and grant schema permissions

--- infra/docker/postgres/init.sql
+++ infra/docker/postgres/init.sql
@@ -13,3 +13,23 @@ GRANT ALL PRIVILEGES ON DATABASE order_db TO order_user;
 GRANT ALL PRIVILEGES ON DATABASE inventory_db TO inventory_user;
 GRANT ALL PRIVILEGES ON DATABASE payment_db TO payment_user;
 GRANT ALL PRIVILEGES ON DATABASE catalog_db TO catalog_user;
+
+\connect order_db;
+GRANT USAGE, CREATE ON SCHEMA public TO order_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT ALL ON TABLES TO order_user;
+
+\connect inventory_db;
+GRANT USAGE, CREATE ON SCHEMA public TO inventory_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT ALL ON TABLES TO inventory_user;
+
+\connect payment_db;
+GRANT USAGE, CREATE ON SCHEMA public TO payment_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT ALL ON TABLES TO payment_user;
+
+\connect catalog_db;
+GRANT USAGE, CREATE ON SCHEMA public TO catalog_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT ALL ON TABLES TO catalog_user;
